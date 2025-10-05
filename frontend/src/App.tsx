4b3a9696be865ca13e7acf02af98060eb0fe02be
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2025-10-05 15:01:15 +0900

    feat(FE): Setup React Router Routes 설정 및 테스트

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,26 @@
+import { Route, Routes } from "react-router";
+import NavBar from "./components/NavBar";
+import {
+    HomePage,
+    LoginPage,
+    ProfilePage,
+    SignUpPage,
+    SettingsPage,
+} from "./pages";
+
 function App() {
-    return <h1 className="text-3xl">Hello World</h1>;
+    return (
+        <div>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,7 @@
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import ProfilePage from "./ProfilePage";
+import SignUpPage from "./SignUpPage";
+import SettingsPage from "./SettingsPage";
+
+export { HomePage, LoginPage, ProfilePage, SignUpPage, SettingsPage };
